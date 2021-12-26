8e80b4c6ecb30af49010594ac9e1707a1e382557
Hugo Guerrier <hugogguerrier@gmail.com>
2021-12-26 12:26:53 +0100

    Create the base for the backend and the frontend

---
+++ backend/src/main/java/fr/bookin/bookin_back/BookinBackApplication.java
@@ -0,0 +1,13 @@
+package fr.bookin.bookin_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookinBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookinBackApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/fr/bookin/bookin_back/BookinBackApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.bookin.bookin_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookinBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
